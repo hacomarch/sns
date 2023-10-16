b47c1d95198cd5aedd01bd236fb97507e83a5541
윤하은 <haco_march@naver.com>
2023-10-16 22:05:29 +0900

    오류인듯

--- src/main/java/com/example/sns/SnsApplication.java
+++ src/main/java/com/example/sns/SnsApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
 public class SnsApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(SnsApplication.class, args);
     }
